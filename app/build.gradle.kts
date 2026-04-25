0ed2eec1a0853a59c2588567358db5f60be9bb2d
Alexandru Calcan <Alex.Calcan@alcagrup.local>
2026-04-25 20:23:31 +0300

    Release v0.6.0 (versionCode 8)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "eu.sancris.cititor"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.5.0"
+        versionCode = 8
+        versionName = "0.6.0"
 
         buildConfigField("String", "GITHUB_OWNER", "\"alexcalcan\"")
         buildConfigField("String", "GITHUB_REPO", "\"sancris_android\"")
